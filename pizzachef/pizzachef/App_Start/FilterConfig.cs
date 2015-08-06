46a27b6650b032b13e5afd9fd504ef42abadc5f0
Tuan <pizzachefpro@gmail.com>
2015-08-06 18:33:47 +0700

    new

---
+++ pizzachef/pizzachef/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace pizzachef
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
