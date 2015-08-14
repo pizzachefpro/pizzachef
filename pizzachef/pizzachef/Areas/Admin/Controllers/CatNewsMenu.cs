72b87b54362b4b3a5ea497ea560bb220dc6e93d4
Tuan <tranhoangtuan.jp@gmail.com>
2015-08-14 10:19:42 +0700

    CatNewMenu, CatProMenu, Layout Admin

---
+++ pizzachef/pizzachef/Areas/Admin/Controllers/CatNewsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using pizzachef.Areas.Admin.Models;
+
+namespace pizzachef.Areas.Admin.Controllers
+{
+    public class CatNewsMenu
+    {
+        private pizzachefdbEntities db = new pizzachefdbEntities();
+
+        public List<String> getCatNewsName()
+        {
+            List<String> a = new List<String>();
+            IEnumerable<string> query = from d in db.CategoryNews
+                                        select d.CatNewsName;
+            foreach (var kq in query)
+            {
+                a.Add(kq.ToString());
+            }
+            return a;
+        }
+    }
+}
---
+++ pizzachef/pizzachef/Areas/Admin/Controllers/CatProMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using pizzachef.Areas.Admin.Models;
+
+namespace pizzachef.Areas.Admin.Controllers
+{
+    public class CatProMenu
+    {
+        private pizzachefdbEntities db = new pizzachefdbEntities();
+        
+        public List<String> getCatProName()
+        {
+            List<String> a = new List<String>();
+            IEnumerable<string> query = from d in db.CategoryProducts
+                                     select d.CatProName;
+            foreach (var kq in query)
+            {
+                a.Add(kq.ToString());
+            }
+            return a;
+        }
+
+        public IEnumerable<int> getCatProID()
+        {
+           
+            IEnumerable<int> query = from d in db.CategoryProducts select d.CatProID;
+            return query;
+        }
+    }
+}
